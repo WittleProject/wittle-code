7d06907981309b4a42f081bdb3e408dd35e53ae7
Davin Rothschild <davinmrothschild@gmail.com>
2015-02-11 00:14:47 -0800

    Committing functioning (if as of yet not really doing anything) navigation drawer. Currently Action Bar text toggle isn't working, so it's commented out.

--- app/src/main/java/com/dev/wittle/PlayActivity.java
+++ app/src/main/java/com/dev/wittle/PlayActivity.java
@@ -1,17 +1,69 @@
 package com.dev.wittle;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class PlayActivity extends ActionBarActivity {
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mItemTitles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        mTitle = mDrawerTitle = getTitle();
+        mItemTitles = getResources().getStringArray(R.array.nav_drawer_array);
+        //mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Can add a drawable drawer shadow
+       // mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        mDrawerList.setAdapter(new ArrayAdapter<>(
+                this, R.layout.drawer_list_item, mItemTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        //TODO: Code to toggle the top navigation - doesn't work yet.
+        /*getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                //R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ){
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            //selectItem(0);
+        }*/
     }
 
 
@@ -35,5 +87,13 @@ public class PlayActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //selectItem(position);
+        }
     }
 }
